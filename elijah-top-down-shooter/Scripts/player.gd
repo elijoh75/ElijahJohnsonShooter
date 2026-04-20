e07cbc2798a5530a5ea42635e746be940f04203c
elijoh75 <elijoh75@kingshigh.school.nz>
2026-04-21 10:55:32 +1200

    PlayerMove and Enemy0.3

--- elijah-top-down-shooter/Scripts/player.gd
+++ elijah-top-down-shooter/Scripts/player.gd
@@ -1,1 +1,17 @@
 extends CharacterBody2D
+
+var speed = 300.0
+
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+
+func _physics_process(delta: float) -> void:
+	var move_dir = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
+	
+	if move_dir != Vector2.ZERO:
+		velocity = speed * move_dir.normalized()
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+
+	move_and_slide()
