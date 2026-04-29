62eaadc624e2d45c37713ba70ab021c730f894d9
elijoh75 <elijoh75@kingshigh.school.nz>
2026-04-30 11:00:10 +1200

    0.4stable
    almost working

--- elijah-top-down-shooter/Scripts/enemy.gd
+++ elijah-top-down-shooter/Scripts/enemy.gd
@@ -1,2 +1,42 @@
 extends CharacterBody2D
 class_name Enemy
+
+var player: Player = null
+
+var speed: float = 100.0
+var direction := Vector2.ZERO
+
+
+func _process(delta: float) -> void:
+	if player !=null:
+		look_at(player.global_position)
+
+func _physics_process(delta: float) -> void:
+	if player != null:
+		var enemey_to_player = (player.global_position - global_position)
+		if enemey_to_player.length() > 50.0:
+			direction = enemey_to_player.normalized()
+		else:
+			direction = Vector2.ZERO
+		
+		
+		if direction != Vector2.ZERO:
+			velocity = speed * direction
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			velocity.y = move_toward(velocity.y, 0, speed)
+		
+		move_and_slide()
+
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	if body is Player:
+		if player == null:
+			player = body
+			print(name + " found the player")
+
+
+func _on_player_detector_body_exited(body: Node2D) -> void:
+	if body is Player:
+		if player != null:
+			player = null
+			print(name + " lost the player")
